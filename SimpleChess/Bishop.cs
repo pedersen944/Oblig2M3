bd3209baadfa681fb556e6b1b12dd21cf1db962c
pedersen944 <mpeders99@gmail.com>
2020-09-17 11:01:59 +0200

    Initial commit

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+     class Bishop : Piece
+    {
+        
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var colMove = fromPosition[0] - toPosition[0];
+            var rowMove = fromPosition[1] - toPosition[1];
+            return Math.Abs(colMove) == Math.Abs(rowMove);
+        }
+
+        public Bishop(string symbol = "LPR") : base(symbol)
+        {
+        }
+    }
+
+
+}
---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChess
+{
+     class Rook : Piece
+    {
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition [0] || fromPosition[1] == toPosition[1];
+        }
+
+        public Rook(string symbol ="TRN") : base(symbol)
+        {
+        }
+    }
+}
